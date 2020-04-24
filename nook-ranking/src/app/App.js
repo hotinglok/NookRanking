6189cc5e8340cb509ea3c0ccb515568007abc8a6
Harvey Peachey <harvey.peachey@bbc.co.uk>
2020-04-24 15:21:18 +0100

    mapped villager data to an array for ease of use

--- nook-ranking/src/app/App.js
+++ nook-ranking/src/app/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 import PageWrapper from '../components/PageWrapper/PageWrapper';
-import villagerData from '../data/villager-data.json';
+import data from '../data/villager-data.json';
 
 function App() {
+  const villagerData = Object.keys(data).map(key => {
+    return data[key];
+  });
   return (
     <div className="App">
       <PageWrapper data={villagerData}/>
