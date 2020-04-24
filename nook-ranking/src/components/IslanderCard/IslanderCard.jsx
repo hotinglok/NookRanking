a0013a24d5a60881acf3afae1c6030c153175e22
Harvey Peachey <harvey.peachey@bbc.co.uk>
2020-04-24 14:02:17 +0100

    Added basic components

---
+++ nook-ranking/src/components/IslanderCard/IslanderCard.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const IslanderCard = ({ name, image }) => {
+  return (
+    <div>hi</div>
+  );
+}
+
+export default IslanderCard;
---
+++ nook-ranking/src/components/RankList/RankList.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const RankList = ({ data }) => {
+  return (
+    <div>hi</div>
+  );
+}
+
+export default RankList;
